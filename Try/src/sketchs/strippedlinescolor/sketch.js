c41d3994e7e93a3645d18a5d82dfa4939b7573cf
Constant Dupuis <constant.dupuis@gmail.com>
2024-07-23 17:08:49 +0200

    Add colored stripped lines sketch

--- Try/src/sketchs/strippedlinescolor/sketch.js
+++ Try/src/sketchs/strippedlinescolor/sketch.js
@@ -1,15 +1,11 @@
 
-//colorScale = chroma.scale(['#FD9B63', '#E7D37F','#81A263', '#365E32']);
-//colorScale = chroma.scale(['#78ABA8', '#C8CFA0','#FCDC94', '#EF9C66']);
-//colorScale = chroma.scale(['#1A5319', '#508D4E','#80AF81', '#D6EFD8']);
-//colorScale = chroma.scale(['#468585', '#50B498','#9CDBA6', '#DEF9C4']);
-//colorScale = chroma.scale(['#131842', '#E68369','#ECCEAE', '#FBF6E2']);
-//colorScale = chroma.scale(['#FF7EE2', '#FFA38F','#FFD18E', '#E9FF97']);
-colorScale = chroma.scale(['#B60071', '#E4003A','#EB5B00', '#FFB200']); // 8/10
-//colorScale = chroma.scale(['#E76F51', '#F4A261','#E9C46A', '#36BA98']); // 5/10
-//colorScale = chroma.scale(['#DD761C', '#FEB941','#FDE49E', '#6DC5D1']); // x/10
 
-
+colorScale01 = chroma.scale(['#B60071', '#E4003A','#EB5B00', '#FFB200']); 
+colorScale02 = chroma.scale(['#FF7EE2', '#FFA38F','#FFD18E', '#E9FF97']); 
+colorScale03 = chroma.scale(['#78ABA8', '#C8CFA0','#FCDC94', '#EF9C66']); 
+colorScale04 = chroma.scale(['#131842', '#E68369','#ECCEAE', '#FBF6E2']); 
+colorScale05 = chroma.scale([ '#E9C46A','#36BA98','#E76F51', '#F4A261']);
+colorScale06 = chroma.scale(['#DD761C', '#FEB941','#FDE49E', '#6DC5D1']);
 
 let color1;
 let color2;
@@ -25,20 +21,65 @@ let noiseScale = 0.1;
 let loadingDiv;
 let firstDraw = true;
 
+let colorPalettes = [];
+
+function setColorPalettes(){
+
+  colorPalettes.push( {
+    color1 : color(0,255,255),
+    color2 : color(255,255,0),
+    color3 : color(255,0,255)
+  });
+
+  colorPalettes.push( {
+    color1 : color(colorScale01(0.0).hex()),
+    color2 : color(colorScale01(0.5).hex()),
+    color3 : color(colorScale01(1.0).hex())
+  });
+
+  colorPalettes.push( {
+    color1 : color(colorScale02(0.0).hex()),
+    color2 : color(colorScale02(0.5).hex()),
+    color3 : color(colorScale02(1.0).hex())
+  });
+
+  colorPalettes.push( {
+    color1 : color(colorScale03(0.0).hex()),
+    color2 : color(colorScale03(0.5).hex()),
+    color3 : color(colorScale03(1.0).hex())
+  });
+
+  colorPalettes.push( {
+    color1 : color(colorScale04(0.0).hex()),
+    color2 : color(colorScale04(0.5).hex()),
+    color3 : color(colorScale04(1.0).hex())
+  });
+
+  colorPalettes.push( {
+    color1 : color(colorScale05(0.0).hex()),
+    color2 : color(colorScale05(0.5).hex()),
+    color3 : color(colorScale05(1.0).hex())
+  });
+
+  colorPalettes.push( {
+    color1 : color(colorScale06(0.0).hex()),
+    color2 : color(colorScale06(0.5).hex()),
+    color3 : color(colorScale06(1.0).hex())
+  });
+}
+
+function selectColorPalette(){
+  console.log(`Color palettes length ${colorPalettes.length}`);
+  let index = Math.floor(random(colorPalettes.length));
+  console.log(`Color palettes index ${index}`);
+  color1 = colorPalettes[index].color1;
+  color2 = colorPalettes[index].color2;
+  color3 = colorPalettes[index].color3;
+}
+
 function setup() {
-
-
-  // color1 = color(0,255,255); // RGB
-  // color2 = color(255,255,0); // RGB
-  // color3 = color(255,0,255); // RGB
-
-  // color1 = color(0,100,100); // RGB
-  // color2 = color(250,250,0); // RGB
-  // color3 = color(180,0,110); // RGB
-
-  color1 = color(colorScale(0.0).hex());
-  color2 = color(colorScale(0.5).hex());
-  color3 = color(colorScale(1.0).hex());
+  setColorPalettes();
+  selectColorPalette();
 
   createCanvas(windowWidth, windowHeight);
   loadingDiv = document.getElementById("loading");
@@ -49,7 +90,6 @@ function windowResized() {
   // Adjust the canvas size when the window is resized
   showLoading();
   resizeCanvas(windowWidth, windowHeight);
-  //myDraw();
 }
 
 function hideLoadingOnFirstDraw() {
@@ -90,17 +130,11 @@ function draw()
     {
       let noiseVal = noise(i*noiseScale, j*noiseScale);
       noiseVal *= noiseVal;
-      //console.log(`noise value ${noiseVal}`);
-      //fill(noiseVal * 255, 0, 0, 255/3);
-      //fill(noiseVal * 255);
-      //rect( i * halfCellWidth,  j * halfCellWidth, halfCellWidth, halfCellWidth);
-      color1.setAlpha((noiseVal * 255)/alphaDivider);
+       color1.setAlpha((noiseVal * 255)/alphaDivider);
       stroke(color1);
-      //stroke(0,255,255,(noiseVal * 255)/1.2);
-      strokeWeight( 1 + random(3));
+       strokeWeight( 1 + random(3));
       draw_square( i * cellWidth,  j * cellWidth, cellWidth, 6 - random(2), random(TWO_PI));
-      //rect( i * cellWidth,  j * cellWidth, cellWidth, cellWidth);
-    }
+     }
   }
 
   noiseSeed(Date.now() + 32165944987);
@@ -115,17 +149,11 @@ function draw()
     {
       let noiseVal = noise(width+i*noiseScale, width+j*noiseScale);
       noiseVal *= noiseVal; 
-      //console.log(`noise value ${noiseVal}`);
-      //fill(noiseVal * 255);
-      //fill(0, noiseVal * 255, 0, 255/3);
-      //rect( width /2 + i * halfCellWidth,  j * halfCellWidth, halfCellWidth, halfCellWidth);
-      //stroke(255,255,0, (noiseVal * 255)/1.2);
-      color2.setAlpha((noiseVal * 255)/alphaDivider);
+        color2.setAlpha((noiseVal * 255)/alphaDivider);
       stroke(color2);
       strokeWeight( 1 + random(3));
       draw_square( i * cellWidth,  j * cellWidth, cellWidth, 6 - random(2), random(TWO_PI));
-      //rect(  i * cellWidth,  j * cellWidth, cellWidth, cellWidth);
-    }
+     }
   }
 
 
@@ -141,17 +169,11 @@ function draw()
       {
         let noiseVal = noise(width+i*noiseScale, width+j*noiseScale);
         noiseVal *= noiseVal; 
-        //console.log(`noise value ${noiseVal}`);
-        //fill(noiseVal * 255);
-        //fill(0, noiseVal * 255, 0, 255/3);
-        //rect( i * halfCellWidth,  height /2 + j * halfCellWidth, halfCellWidth, halfCellWidth);
-        //stroke(255,0,255, (noiseVal * 255)/1.2);
         color3.setAlpha((noiseVal * 255)/alphaDivider);
         stroke(color3);
         strokeWeight( 1 + random(3));
         draw_square( i * cellWidth,  j * cellWidth, cellWidth, 6 - random(2), random(TWO_PI));
-        //rect( i * cellWidth,  + j * cellWidth, cellWidth, cellWidth);
-      }
+       }
     }
  
 }
@@ -178,6 +200,3 @@ function mousePressed(event) {}
 
 function mouseDragged(event) {}
 
-
-// function mouseReleased(event) {
-// }
